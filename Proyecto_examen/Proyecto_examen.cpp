d888779bc333293aab7ee5b1dd7c58a38330e7fe
CESAR ROLDAN MARTINEZ <cesar.roldan@alumno.buap.mx>
2024-11-08 00:47:24 -0600

    yu

--- Proyecto_examen/Proyecto_examen.cpp
+++ Proyecto_examen/Proyecto_examen.cpp
@@ -16,32 +16,41 @@ int pasos;
 int main()
 {
     cout << "Invierte en ti mismo y cosecha los frutos de una vida mas saludable y feliz." << endl;
-    cout << "\nDigite su edad:" << endl;
+
+ 
+    cout << "\nDigite su edad: ";
     cin >> edad;
     per.setEdad(edad);
 
-    cout << "\nDigite su peso:" << endl;
+    cout << "\nDigite su peso: ";
     cin >> peso;
     per.setPeso(peso);
 
-    cout << "\nDigite su altura: " << endl;
+    cout << "\nDigite su altura: ";
     cin >> altura;
     per.setAltura(altura);
 
-    cout << "\nEspecifique sexo: (M para masculino  y F para femenino)" << endl;
+    cout << "\nEspecifique sexo (M para masculino y F para femenino): ";
     cin >> sexo;
     per.setSexo(sexo);
 
-    cout << "Para una persona  de sexo "<< per.getSexo() << ", " << per.getEdad() << "anios, " << per.getPeso() << " KG, y "<< per.getAltura()<< "m de altura, Su indice de masa corporal es  " << per.IMC() << endl;
+    cout << "\nPara una persona de sexo " << per.getSexo() << ", " << per.getEdad()
+        << " años, " << per.getPeso() << " KG, y " << per.getAltura() << " m de altura" << endl;
+
+    cout << "\nDados los datos introducidos su IMC (Indice de Masa Corporal): "
+        << per.IMC() << endl;
+
+ 
+    per.IMC(); 
+    cout << "Y el estado en el que se encuentra es de: " << actv.CatIMC() << endl;
+
     
-    cout << "Muy bien ahora con estos datos recopilados se comenzara a dar recomendaciones " << endl;
-    cout << "para ello introduzca el numero de pasos que da aproximadamente al dia" << endl;
+    cout << "Muy bien, ahora con estos datos recopilados se comenzar a dar recomendaciones." << endl;
+    cout << "Para ello, introduzca el número de pasos que da aproximadamente al da: ";
     cin >> pasos;
-    act.calConsumidas(pasos);
-    cout << "\nCalorias consumidas: " << act.calConsumidas() << "Kcal" << endl;
+    actv.calConsumidas(peso, altura, pasos);
 
-
+    cout << "\nCalorias consumidas: " << actv.getcalorias() << " Kcal" << endl;
 
     return 0;
-    system("pause");
 }
